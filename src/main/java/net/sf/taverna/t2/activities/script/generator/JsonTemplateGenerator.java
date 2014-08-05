9164c0752e58af570a980114e7da677b4c05e38c
Alan R Williams <alan.r.williams@manchester.ac.uk>
2014-08-05 15:05:20 +0100

    Changed the template

--- src/main/java/net/sf/taverna/t2/activities/script/generator/JsonTemplateGenerator.java
+++ src/main/java/net/sf/taverna/t2/activities/script/generator/JsonTemplateGenerator.java
@@ -3,13 +3,18 @@
  */
 package net.sf.taverna.t2.activities.script.generator;
 
+import java.util.List;
+
 import net.sf.taverna.t2.activities.script.ScriptActivityConfigurationBean;
+import net.sf.taverna.t2.workflowmodel.processor.activity.config.ActivityInputPortDefinitionBean;
 
 /**
  * @author alanrw
  *
  */
 public class JsonTemplateGenerator extends ScriptGenerator {
+	
+	private static final String lineSeparator = System.getProperty("line.separator");
 
 	/* (non-Javadoc)
 	 * @see net.sf.taverna.t2.activities.script.generator.ScriptGenerator#canGenerateFrom(net.sf.taverna.t2.activities.script.ScriptActivityConfigurationBean)
@@ -24,7 +29,30 @@ public class JsonTemplateGenerator extends ScriptGenerator {
 	 */
 	@Override
 	public String generateFrom(ScriptActivityConfigurationBean bean) {
-		return "Hello Mister Snoop";
+		
+		final StringBuffer sb = new StringBuffer();
+		
+		List<ActivityInputPortDefinitionBean> inputPorts = bean.getInputPortDefinitions();
+		
+		sb.append("## Auto-generated template");
+		sb.append(lineSeparator);
+		sb.append("{");
+		
+			String separator = "";
+			for (final ActivityInputPortDefinitionBean port : inputPorts) {
+				String portName = port.getName();
+				sb.append(separator);
+				separator = ",";
+				
+				sb.append(String.format("%s    \"", lineSeparator));
+
+				sb.append(portName);
+				sb.append("\": $");
+				sb.append(portName);
+			}
+		sb.append(String.format("%s}", lineSeparator));
+		
+		return sb.toString();
 	}
 
 	/* (non-Javadoc)
